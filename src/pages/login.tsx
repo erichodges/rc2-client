d25754af3c0b86ea3828095fb4f158d8cdc75a59
Eric Hodges <ehodges2004@gmail.com>
2021-01-01 14:06:05 -0800

    Setup: Added useAuthDispatch to login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,22 +4,27 @@ import Link from "next/link";
 import Axios from "axios";
 import InputGroup from "../components/InputGroup";
 import { useRouter } from "next/router";
+import { useAuthDispatch } from "../context/auth";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<any>({});
 
+  const dispatch = useAuthDispatch();
   const router = useRouter();
 
   const submitForm = async (event: FormEvent) => {
     event.preventDefault();
 
     try {
-      await Axios.post("/auth/login", {
+      const res = await Axios.post("/auth/login", {
         username,
         password,
       });
+
+      dispatch("LOGIN", res.data);
+
       router.push("/");
     } catch (err) {
       console.log(err);
